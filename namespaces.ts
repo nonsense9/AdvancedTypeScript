cf80295ffe6f427be75538bec156c495815a3b78
nonsense9 <67061900+nonsense9@users.noreply.github.com>
2020-09-04 18:11:10 +0300

    Add files via upload
    this file exports namespaces from file form_namespaces

---
+++ namespaces.ts
@@ -0,0 +1,23 @@
+/// <reference path="form_namespace.ts" />
+
+namespace Form {
+class MyForm {
+  private type: FormType = 'inline'
+  private state: FormState = 'active'
+
+  constructor(public email: string) {
+
+  }
+
+  getInfo(): FormInfo {
+    return {
+      type: this.type,
+      state: this.state
+    }
+  }
+}
+
+export const form = new MyForm ('v@mail.ru')
+
+}
+console.log(Form.form);
