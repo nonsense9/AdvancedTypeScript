442fdf8e4e6fe53f45c7ea5b8f6b73693f395d88
nonsense9 <67061900+nonsense9@users.noreply.github.com>
2020-09-04 16:59:18 +0300

    Add files via upload
    decorators

---
+++ decorators.ts
@@ -0,0 +1,150 @@
+// function Log(constructor: Function) {
+//   console.log(constructor);
+// }
+
+// function Log2(target: any, propName: string | Symbol) {
+//   console.log(target);
+//   console.log(propName); 
+// }
+
+// function Log3(target: any, propName: string | Symbol, descriptor: PropertyDescriptor) {
+//   console.log(target);
+//   console.log(propName);
+//   console.log(descriptor);  
+// }
+
+// @Log
+// class Component {
+//   @Log2
+//   name: string
+
+//   @Log3
+//   get componentName() {
+//     return this.name;
+//   }
+
+//   constructor(name: string) {
+//     this.name = name
+//   }
+  
+// @Log3
+//   logName(): void {
+//     console.log(`Component Name: ${this.name}`);
+//   }
+// }
+
+// =======================
+
+// interface ComponentDecorator {
+//   selector: string
+//   template: string
+// }
+
+// function Component(config: ComponentDecorator) {
+//   return function
+//     <T extends { new(...args: any[]): object}>
+//   (Constructor: T) {
+//     return class extends Constructor {
+//       constructor(...args: any[]) {
+//         super(...args)
+
+//         const el = document.querySelector(config.selector)!
+//         el.innerHTML = config.template // dont work but as it should :(  https://www.youtube.com/watch?v=7NU6K4170As
+//         console.log(el);
+//       }
+//     }
+//   }
+// }
+
+// function Bind(_:any, _2: any, descriptor: PropertyDescriptor): PropertyDescriptor {
+//   const original = descriptor.value
+
+//   return {
+//     configurable: true,
+//     enumerable: false,
+//     get() {
+//       return original.bind(this)
+//     }
+//   }
+// }
+
+// @Component({
+//   selector: '#card',
+//   template: `
+//     <div class="card">
+//       <div class="card-content">
+//         <span class="card-title">Card Component</span>
+//       </div>
+//     </div>
+//   `
+// })
+
+// class CardComponent {
+//   constructor(public name: string) {
+//   }
+
+//   @Bind
+//   logName(): void {
+//     console.log(`Component Name: ${this.name}`);
+//   }
+// }
+
+// const card = new CardComponent('Card Component')
+
+// const btn1 = document.querySelector('#btn')!
+
+// btn1.addEventListener('click', card.logName)
+
+// ======================
+
+type ValidatorType = 'required' | 'email'
+
+interface ValidatorConfig {
+  [prop: string]: {
+    [validateProp: string]: ValidatorType 
+  }
+}
+
+const validators: ValidatorConfig = {
+
+}
+
+function Required(target: any, propName: string) {
+  validators[target.constructor.name] = {
+    ...validators[target.constructor.name],
+    [propName]: 'required'
+  }
+
+  function validate(obj: any): boolean {
+    const objConfig = validators[obj.constructor.name]
+    if (!objConfig) {
+      return true
+    }
+    let isValid = true
+    Object.keys(objConfig).forEach(key => {
+      if (objConfig[key] === 'required') {
+        isValid = isValid && !!obj[key]
+      }
+    })
+    return isValid
+  }
+}
+
+class Form {
+  @Required
+  public email: string | void
+
+  constructor(email?: string) {
+    this.email = email
+  }
+}
+
+const form = new Form('v@mail.ru')
+
+if (validate(form)) { // <= this one dont work, but it should, i dont know why it doesnt :(
+  console.log('Valid: ', form); // https://www.youtube.com/watch?v=7NU6K4170As
+} else {
+  console.log('Validation error'); 
+}
+
+console.log(form);
